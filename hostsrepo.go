79abd3b107df6c51b1dba9c8f3199af36d5ad6e4
Uchio KONDO <udzura@pepabo.com>
2016-03-28 16:05:08 +0900

    First hosts serve

--- hostsrepo.go
+++ hostsrepo.go
@@ -1,6 +1,12 @@
 package hostsrepo
 
 import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,13 +16,55 @@ type Host struct {
 }
 type Hosts []*Host
 
+var spaces = regexp.MustCompile("\\s+")
+
+// XXX: re-invent of https://golang.org/src/net/hosts.go
+func FindAllHosts(hostsFile string) (Hosts, error) {
+	if hostsFile == "" {
+		hostsFile = "/etc/hosts"
+	}
+
+	hosts := Hosts{}
+	fp, err := os.Open(hostsFile)
+	if err != nil {
+		return hosts, err
+	}
+	defer fp.Close()
+
+	scanner := bufio.NewScanner(fp)
+	for scanner.Scan() {
+		ent := spaces.Split(strings.TrimSpace(scanner.Text()), -1)
+		if len(ent) < 2 {
+			fmt.Fprintf(os.Stdin, "Error hosts entry: %v\n", ent)
+		} else if strings.HasPrefix(ent[0], "#") {
+			fmt.Fprintf(os.Stdin, "Entry is commented out: %v\n", ent)
+		} else {
+			ip := ent[0]
+			for _, hn := range ent[1:] {
+				if strings.HasPrefix(hn, "#") {
+					break
+				}
+				hosts = append(hosts, &Host{Name: hn, IPAddr: ip})
+			}
+		}
+
+	}
+	if err := scanner.Err(); err != nil {
+		return hosts, err
+	}
+
+	return hosts, nil
+}
+
 func RunServer() error {
 	r := gin.Default()
 	r.GET("/hosts", func(c *gin.Context) {
-		hosts := Hosts{
-			{Name: "localhost", IPAddr: "127.0.0.1"},
+		hosts, err := FindAllHosts("")
+		if err != nil {
+			c.JSON(503, err)
+		} else {
+			c.JSON(200, hosts)
 		}
-		c.JSON(200, hosts)
 	})
 	return r.Run("0.0.0.0:10808")
 }
